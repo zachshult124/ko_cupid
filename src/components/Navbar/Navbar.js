b40adb626b884954df8a016648d749ecf1170d24
Nicolas Cueva <nicolascueva@outlook.com>
2018-10-08 12:54:02 -0500

    adds frontend template

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
+const Navbar = props => (
+  <nav className="navbar navbar-expand-lg navbar-light">
+    <Link className="navbar-brand font-weight-bold text-white" to="/">
+      KO Cupid
+    </Link>
+    <div className="">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <Link
+
+            to="/"
+            className={
+              window.location.pathname === "/" || window.location.pathname === "/about"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Home
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/discover"
+            className={
+              window.location.pathname === "/discover"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Matches
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link
+            to="/search"
+            className={
+              window.location.pathname === "/search"
+                ? "nav-link active"
+                : "nav-link"
+            }
+          >
+            Ref
+          </Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
+
+export default Navbar;
